cc8d8daa53100c11f6c929ff52b7dd44a0a6ec07
rabiul155 <siyamict@gmail.com>
2024-05-20 23:08:59 +0600

    update

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const app: Application = express();
 app.use(express.json());
 app.use(cors());
 
-// application routes
+// application routes uses
 app.use('/api/v1/students', StudentRoutes);
 
 const getAController = (req: Request, res: Response) => {
