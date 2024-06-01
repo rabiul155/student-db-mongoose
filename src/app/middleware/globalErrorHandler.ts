0819e58c04b52e3da05a86aab8b0ff3c08117f2d
rabiul155 <siyamict@gamil.com>
2024-06-01 14:33:35 +0600

    fix

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -1,16 +1,46 @@
-import { NextFunction, Request, Response } from 'express';
+import { ErrorRequestHandler } from 'express';
+import { ZodError } from 'zod';
 
-export const globalErrorHandler = (
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction,
+type ErrorSourceType = {
+  path: string | number;
+  message: string;
+};
+
+const handleZodError = (err) => {
+  const error = {};
+};
+
+export const globalErrorHandler: ErrorRequestHandler = (
+  err,
+  req,
+  res,
+  next,
 ) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || 'Something went wrong';
+  //default values
+  let statusCode = err.statusCode || 500;
+  let message = err.message || 'Something went wrong';
+  let errorSource: ErrorSourceType[] = [
+    {
+      path: '',
+      message: 'Something went wrong',
+    },
+  ];
+
+  if (err instanceof ZodError) {
+    statusCode = 400;
+    message = err.message;
+    errorSource = [
+      {
+        path: '500',
+        message: 'hell',
+      },
+    ];
+  }
+
   return res.status(statusCode).json({
     success: false,
     message,
-    err,
+    errorSource,
+    error: err,
   });
 };
